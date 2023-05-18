884481360e663bd67ab8bacb9a4d219bf84b0165
Joseph Herrera <josephherreraleon@gmail.com>
2023-05-17 20:17:07 -0600

    Card, card container and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Card } from "./card";
+import './card.css';
+
 export default function App() {
 
   return (
     <>
-      <h1>Pokedex</h1>
-      <div className="card-display">       
-        
-      </div>
-    </>
+    <h1>Pokedex</h1>
+    <div className="CardDisplay">
+    {Array.from({ length: 9 }).map((_, index) => (
+      <Card
+        key={index}
+        img="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+        alt="Bulbasaur"
+        title="Bulbasaur"
+      />
+    ))}
+  </div>
+</>
+
   )
 }
