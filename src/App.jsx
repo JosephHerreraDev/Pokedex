62bc89db89853b3bad0428e46be811698f97a9df
Joseph Herrera <josephherreraleon@gmail.com>
2023-05-18 19:28:37 -0600

    Connect API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { Card } from "./card";
-import './card.css';
+import {PokemonList} from "./pokemonList";
+
 
 export default function App() {
 
   return (
     <>
     <h1>Pokedex</h1>
-    <div className="CardDisplay">
-    {Array.from({ length: 9 }).map((_, index) => (
-      <Card
-        key={index}
-        img="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
-        alt="Bulbasaur"
-        title="Bulbasaur"
-      />
-    ))}
-  </div>
+    <PokemonList />
 </>
 
   )
