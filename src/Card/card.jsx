9a6a6bf92d1d61d37480247e1d59d4f5763e5808
Joseph Herrera <josephherreraleon@gmail.com>
2023-06-24 19:58:35 -0600

    clean cars.jsx

--- src/Card/card.jsx
+++ src/Card/card.jsx
@@ -7,33 +7,6 @@ export function Card({ img, alt, name }) {
     navigateTo(`/information/${name}`);
   }
 
-  // function handleClick() {
-  //   const pokemonName = name;
-
-  //   async function getPokemon() {
-  //     return fetchPokemon(pokemonName);
-  //   }
-
-  //   getPokemon()
-  //     .then(({ data }) =>
-  //       navigateTo(`/information/${pokemonName}`, {
-  //         state: {
-  //           img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${String(
-  //             data.id
-  //           ).padStart(3, "0")}.png`,
-  //           alt: pokemonName,
-  //           title: pokemonName,
-  //           exp: data.base_experience + "xp",
-  //           hp: data.stats[0].base_stat,
-  //           attack: data.stats[1].base_stat,
-  //           defense: data.stats[2].base_stat,
-  //           specialAttack: data.stats[3].base_stat,
-  //         },
-  //       })
-  //     )
-  //     .catch((error) => console.error(error));
-  // }
-
   return (
     <div className="Card" onClick={handleClick}>
       <img className="CardImage" src={img} alt={alt} />
